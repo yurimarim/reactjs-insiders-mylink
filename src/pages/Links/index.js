eca0326226cf417498f90bb1030a7b9a7d76b1b5
yurimarim <yurimarim@hotmail.com>
2022-02-21 12:59:47 -0300

    fix: deleteLinks functions and emptyList treatment

--- src/pages/Links/index.js
+++ src/pages/Links/index.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import './links.css'
-import { getLinkSave } from '../../services/storeLinks'
+import { getLinkSave, deleteLink } from '../../services/storeLinks'
 import { FiArrowLeft, FiLink, FiTrash } from 'react-icons/fi'
 import { Link } from 'react-router-dom'
 import { ModalItem } from '../../components/ModalItem'
@@ -9,13 +9,14 @@ export function Links() {
   const [myLinks, setMyLinks] = useState([])
   const [data, setData] = useState([])
   const [showModal, setShowModal] = useState(false)
+  const [emptyList, setEmptyList] = useState(false)
 
   useEffect(() => {
     async function getLinks() {
       const result = await getLinkSave('@encurtaLink')
 
       if (result.length === 0) {
-        console.log('lista vazia')
+        setEmptyList(true)
       }
 
       setMyLinks(result)
@@ -29,8 +30,14 @@ export function Links() {
     setShowModal(true)
   }
 
-  function handleDeleteLink(id) {
-    console.log('voce clicou no id ' + id)
+  async function handleDeleteLink(id) {
+    const result = await deleteLink(myLinks, id)
+
+    if (result.length === 0) {
+      setEmptyList(true)
+    }
+
+    setMyLinks(result)
   }
 
   return (
@@ -41,6 +48,12 @@ export function Links() {
         </Link>
         <h1>Meus Links</h1>
       </div>
+
+      {emptyList && (
+        <div className="links-item">
+          <h2 className="empty-text">Sua lista está vazia... 😭</h2>
+        </div>
+      )}
 
       {myLinks.map(link => (
         <div key={link.id} className="links-item">
